22ec4c8001810659461d884671aebc11ebfec31f
Viktoriia <vis22chu@gmail.com>
2020-10-07 00:05:48 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('');
+  for (var i = 0; i < str.length; i++) {
+    for (var [curr, next] of bracketsConfig) {
+      if (curr === str[i] && next === str[i+1]) {
+        str.splice(i, 2);
+        i -= 2;
+      }
+    }
+  }
+  return (str.length === 0);
 }
